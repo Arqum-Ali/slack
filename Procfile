463cbb1288adb0786a6c52a27e7ff7abc3a67eb3
Arqum-Ali <140241880+Arqum-Ali@users.noreply.github.com>
2024-07-02 01:21:44 +0500

    2 commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn slackproject.wsgi:application --workers 3 --timeout 60
+
--- slackproject/settings.py
+++ slackproject/settings.py
@@ -37,10 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'slackapp',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
