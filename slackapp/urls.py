b31daba7daf508c1c01c3a8329451b195b9898da
Arqum-Ali <140241880+Arqum-Ali@users.noreply.github.com>
2024-07-02 17:15:04 +0500

    8 commit

--- slackapp/urls.py
+++ slackapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login, name='login'),
+    path('hello/', views.hello, name='hello'),
     path('callback/', views.oauth_callback, name='oauth_callback'),
 ]
