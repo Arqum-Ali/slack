cf23e58105f31e15b8d683d256efba69bf0f25fb
Arqum-Ali <140241880+Arqum-Ali@users.noreply.github.com>
2024-07-03 17:14:30 +0500

    add the things like add member remvoe the memeber find the member in channel

--- slackapp/urls.py
+++ slackapp/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     # path('hello/', views.hello, name='hello'),
     path('callback/', views.oauth_callback, name='oauth_callback'),
     path('create_channel/', views.create_channel, name='create_channel'),
+    path('list_channels/', views.list_channels, name='list_channels'),
+    path('add_channel_member/', views.add_channel_member, name='add_channel_member'),
+    path('remove_channel_member/', views.remove_channel_member, name='remove_channel_member'),
+    path('get_channel_members/', views.get_channel_members, name='get_channel_members'),
 
 ]
