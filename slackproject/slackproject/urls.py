c35f5a29ca253e385330ad1ed20b399bcf45b305
Arqum-Ali <140241880+Arqum-Ali@users.noreply.github.com>
2024-08-22 20:37:41 +0500

    Resolved merge conflicts and finalized merge

--- slackproject/slackproject/urls.py
+++ slackproject/slackproject/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('slack/', include('slackapp.urls')),
+    path('', include('slackapp.urls')),
 
 ]
